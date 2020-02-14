d2820f047176796f548258c52e2114dd6a46a821
Felix Strobel <mail@felixstrobel.de>
2020-02-14 10:20:36 +0100

    another try to create the polls app

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
 from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 class Question(models.Model):
-    def __str__(self):
-        return self.question_text
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
 
 class Choice(models.Model):
-    def __str__(self):
-        return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
