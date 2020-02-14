7152845206dd80a66e39d70c72aa5d5f51ae9ad6
Felix Strobel <mail@felixstrobel.de>
2020-02-14 11:50:56 +0100

    add very simlple views for polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 from django.http import HttpResponse
 
@@ -8,6 +10,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -15,4 +19,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text
+        return self.choice_text
