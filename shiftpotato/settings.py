033db2be4ed3b101e1bb53b80f08f8e269221cc2
Felix Strobel <mail@felixstrobel.de>
2020-02-29 22:36:04 +0100

    set DEBUG to False

--- shiftpotato/settings.py
+++ shiftpotato/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '*']
 
